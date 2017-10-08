55bb23f446d23373afd2f5b4fd03d5cf352a86c3
gilderjw <gilderjw@rose-hulman.edu>
2017-10-08 19:56:09 -0400

    added file for regression testing

---
+++ src/test/java/RegressionTesting.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+public class RegressionTesting {
+
+	@Test
+	public void testGet() {
+
+	}
+
+	@Test
+	public void testPost() {
+
+	}
+
+	@Test
+	public void testDelete() {
+
+	}
+
+	@Test
+	public void testPut() {
+
+	}
+
+	@Test
+	public void testHead() {
+
+	}
+}
