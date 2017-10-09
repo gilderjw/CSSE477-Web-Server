ed0dcd8fb831067ef26e97f34c24e81f84345bf8
gilderjw <gilderjw@rose-hulman.edu>
2017-10-09 12:49:35 -0400

    maybe fixed tests?

--- src/test/java/RegressionTesting.java
+++ src/test/java/RegressionTesting.java
@@ -25,7 +25,7 @@ public class RegressionTesting {
 		try {
 
 			// Create connection
-			URL url = new URL("http://localhost:8080/" + file);
+			URL url = new URL("http://localhost:1337/" + file);
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod(type);
 
@@ -64,7 +64,7 @@ public class RegressionTesting {
 		// TODO: Server configuration, ideally we want to read these from an
 		// application.properties file
 		String rootDirectory = "webtest";
-		int port = 8080;
+		int port = 1337;
 
 		// Create a run the server
 		this.server = new Server(rootDirectory, port);
