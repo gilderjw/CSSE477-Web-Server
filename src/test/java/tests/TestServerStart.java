c53f6564bb0a4ed67960f14cc67266e6bf68ce65
gilderjw <gilderjw@rose-hulman.edu>
2017-10-28 20:17:52 -0400

    Added restore script if build fails

---
+++ src/test/java/tests/TestServerStart.java
@@ -0,0 +1,39 @@
+package tests;
+import org.junit.Test;
+
+import server.Server;
+
+/*
+ * TestServerStart.java
+ * Oct 8, 2017
+ *
+ * Copyright (C) 2015 Chandan Raj Rupakheti
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ */
+
+/**
+ *
+ * @author Chandan R. Rupakheti (rupakhet@rose-hulman.edu)
+ */
+public class TestServerStart {
+
+	@Test
+	public void test() throws InterruptedException {
+		Server serv = new Server("web", 8080);
+		serv.stop();
+		while (!serv.isStoped()) {
+			Thread.sleep(100);
+		}
+	}
+}
